c79a020bcb11781d022848ac1078641a25d19b64
Eldar Djafarov <djkojb@gmail.com>
2019-02-27 09:48:19 +0100

    feat: add flatten and pick

--- gql-json-slicer.js
+++ gql-json-slicer.js
@@ -5,10 +5,22 @@ module.exports = function gqlSlice(query, object){
 }
 
 function objectSlicer(obj, schema) {
-  if (Array.isArray(obj)) return obj.map(el => objectSlicer(el, schema));
+  if (Array.isArray(obj)) {
+    let arrayResult = obj.map(el => objectSlicer(el, schema));
+    if(schema.arguments) {
+      const args = argumentsToObject(schema.arguments);
+      if(args.limit) {
+        arrayResult = arrayResult.slice(0, args.limit);
+      }
+      if(args.pick) {
+        arrayResult = arrayResult.reduce((r, ar) => ([...r, flattenObject(ar)]), []);
+      }
+    }
+    return arrayResult;
+  }
   
   if (schema.selectionSet && schema.selectionSet.selections) {
-    return schema.selectionSet.selections.reduce((r, s) => {
+    let objectResult = schema.selectionSet.selections.reduce((r, s) => {
       if (obj[s.name.value] && typeof obj[s.name.value] === 'object') {
         r[s.name.value] = objectSlicer(obj[s.name.value], s);
       } else {
@@ -16,6 +28,27 @@ function objectSlicer(obj, schema) {
       }
       return r;
     }, {});
+    if(schema.arguments) {
+      const args = argumentsToObject(schema.arguments);
+      if(args.flatten) {
+        const keys = Object.keys(objectResult);
+        const isArrays =  keys.reduce((r, or) => !r ? r : Array.isArray(objectResult[or]), true);
+        
+        objectResult = isArrays 
+                        ? keys.reduce((r, k) => [].concat(r, objectResult[k]) ,[]) 
+                        : keys.reduce((r, k) => Object.assign({}, r, objectResult[k]) ,{});
+      }
+    }
+    return objectResult;
   }
   return obj;
+}
+
+function flattenObject(o) {
+  const keys = Object.keys(o);
+  return keys.length === 1 ? o[keys[0]] : keys.map(k => o[k]);
+}
+
+function argumentsToObject(args) {
+  return args.reduce((r, a) => ({...r, [a.name.value]: a.value.value}), {})
 }
--- gql-json-slicer.test.js
+++ gql-json-slicer.test.js
@@ -74,3 +74,115 @@ describe('gqlSlice', () => {
   })
 
 })
+
+
+describe('gqlSlice pick', () => {
+  var data1 = {
+    field1: [{
+      a:1,
+      b:2
+    },
+    {
+      a:3,
+      b:4
+    },{
+      a:5,
+      b:6
+    }]
+  }
+  test('should pick the array, to array of arrays', () => {
+    expect(gqlSlice(`{
+      field1(pick: true) {
+        a
+        b
+      }
+    }`, data1)).toMatchSnapshot();
+  })
+  test('should pick the array to array', () => {
+    expect(gqlSlice(`{
+      field1(pick: true) {
+        a
+      }
+    }`, data1)).toMatchSnapshot();
+  })
+})
+
+describe('gqlSlice limit', () => {
+  var data1 = {
+    field1: [1,2,3,4,5]
+  }
+  test('should limit the array', () => {
+    expect(gqlSlice(`{
+      field1(limit: 2)
+    }`, data1)).toMatchSnapshot();
+  })
+})
+
+describe('gqlSlice flatten', () => {
+  var data1 = {
+    field1: {
+      toFlat1: {
+        a:1,
+        b:2
+      },
+      toFlat2: {
+        c:3
+      }
+    }
+  }
+  test('should flatten the object', () => {
+    expect(gqlSlice(`{
+      field1(flatten: true) {
+        toFlat1 {
+          a
+          b
+        }
+        toFlat2 {
+          c
+        }
+      }
+    }`, data1)).toMatchSnapshot();
+  })
+  var data2 = {
+    field1: {
+      toFlat1: [{
+        a:1,
+        b:2
+      },{
+        a:3,
+        b:4
+      }
+      ],
+      toFlat2: [{
+        c:3
+      },{
+        c:6
+      }]
+    }
+  }  
+  test('should flatten the array', () => {
+    expect(gqlSlice(`{
+      field1(flatten: true) {
+        toFlat1 {
+          a
+          b
+        }
+        toFlat2 {
+          c
+        }
+      }
+    }`, data2)).toMatchSnapshot();
+  })
+  test('should flatten the array with pick', () => {
+    expect(gqlSlice(`{
+      field1(flatten: true) {
+        toFlat1(pick: true) {
+          a
+        }
+        toFlat2(pick: true) {
+          c
+        }
+      }
+    }`, data2)).toMatchSnapshot();
+  })
+})
